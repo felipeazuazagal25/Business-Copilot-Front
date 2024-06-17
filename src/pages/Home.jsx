d8cb3635d461784ebeb7b13042561bfd3f286dad
Felipe Azua Zagal <felipeazuazagal@Felipes-MacBook-Pro.local>
2024-06-17 15:50:50 +0200

    testing for deployment 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,7 @@ const Home = () => {
   return (
     <div>
       <div>This is a new text for the home page</div>
-      <div>This is a new text for the home page</div>
-      <div>This is a new text for the home page</div>
-      <div>This is a new text for the home page</div>
-      <div>This is a new text for the home page</div>
+      <div>Testing new test</div>
     </div>
   );
 };
